128a5aa1ce12c3a68417937d0a8a6edb8e9679ed
Asmit Das Joy <asmitjoy17@gmail.com>
2020-04-30 00:39:39 +0600

    project-2

---
+++ hello_you-p2.py
@@ -0,0 +1,23 @@
+#Ask user for name
+
+name = input("What is your name ? : ")
+
+#Ask the user for age
+
+age = input("How old are you ? : ")
+
+#Ask user for city
+city = input("What city do you live in ? : ")
+
+#Ask user what they enjoy
+
+love = input("What do you love doing ? : ")
+
+#Create output text
+
+string  = "Your name is {} and you are {} years old. You live in {} and you love {}"
+output =  string.format(name,age,city,love)
+ 
+#Print output to screen
+
+print(output)
