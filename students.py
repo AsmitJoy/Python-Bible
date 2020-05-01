fb0e2866e954f36cf886986b9efadc10b8e3da11
Asmit Das Joy <asmitjoy17@gmail.com>
2020-05-01 14:16:45 +0600

    python_dictonary

---
+++ students.py
@@ -0,0 +1,11 @@
+students = {
+            "Alice":{"id":"001","age":20,"grade":"A"},
+            "Bob":{"id":"002","age":22,"grade":"B"},
+            "Claire":{"id":"003","age":30,"grade":"F"},
+            "Dan":{"id":"004","age":50,"grade":"D"},
+            "Emma":{"id":"005","age":17,"grade":"E"}
+    }
+
+print(students["Dan"]["age"])
+
+print(students['Emma']["id"], students['Emma']["grade"])
